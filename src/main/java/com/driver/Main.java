da799a3a6ddb2bbb35ea602bf79010e1a5a1f424
nikhil vanamala <nickhil215@gmail.com>
2022-12-10 22:41:01 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
+    public static class product{
+        public int product(int x,int y){
+            return x*y;
+        }
+        public  int product(int x, int y,int z){
+            return x*y*z;
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+    }
+    public static void main(String[]args){
+
+        int y=1,z=1,x=1;
+        double a=1.0,b=1.0;
+        product p=new product();
+        p.product(x,y);
+        p.product(x,y,z);
+        p.product(a,b);
+    }
 
 }
